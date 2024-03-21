71de70c91fd27c60664869cd9205638ba35e6c28
yonggi1234 <87134708+yonggi1234@users.noreply.github.com>
2024-03-22 06:45:03 +0900

    Add files via upload
    sass 적용

---
+++ components/App.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+
+
+class App extends Component {
+  
+  render(){
+    
+
+    return(
+      <div>scss 적용 확인</div>  
+   )
+  }
+}
+
+
+
+export default App;
