ea4e103b399ecfaaf3f0fb71409ee06873c9633a
yonggi1234 <littedeer108@gmail.com>
2024-05-29 17:40:00 +0900

    디렉토리 정리

--- src/components/Board/VideoBoard.jsx
+++ src/components/Board/VideoBoard.jsx
@@ -1,21 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { fetchVideos } from '../Fetch/fetchVideo';
-import ScreenRef from '../Board/ScreenRef';
+import React, { useState } from 'react';
 import Test from '../Board/test';
 
 function VideoBoard() {
-    const [videos, setVideos] = useState([]);
-
-    useEffect(() => {
-        fetchVideos()
-            .then(data => setVideos(data))
-            .catch(error => console.error('Error setting videos:', error));
-    }, []);
-
     return (
         <div>
             <Test/>
-            {/* <ScreenRef videos={videos} /> */}
         </div>
     );
 }
--- src/components/Board/body.jsx
+++ src/components/Board/body.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import "../../style/body.css"
 import VideoBoard from './VideoBoard';
-import SSE from '../../components/SSE/SSEVideo';
 
 function Body() {
 
     
     return (
             <div className="screen">
-                {/* <SSE/> */}
                 <VideoBoard/>
             </div>
     );
